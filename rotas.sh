8d166ff00ec714a34419941ddb64ba5fd86db857
GabrielFerreira7 <gabrielbicalhoferreira@gmail.com>
2022-03-06 13:36:47 -0300

    manifest e scripts de instalacao

--- rotas.sh
+++ rotas.sh
@@ -1,4 +1,9 @@
+# Redirecionando a porta 30000 da VM para a porta 30000 do minikube
 sudo iptables -A PREROUTING -t nat -i enp0s3 -p tcp --dport 30000 -j DNAT --to 192.168.49.2:30000
-
-
+# aceitando o redirecionamento
 sudo iptables -A FORWARD -p tcp -d 192.168.49.2 --dport 30000 -j ACCEPT
+
+#Redirecionando a porta 80 da VM para a porta 80 do minikube
+sudo iptables -A PREROUTING -t nat -i enp0s3 -p tcp --dport 80 -j DNAT --to 192.168.49.2:80
+# aceitando o redirecionamento
+sudo iptables -A FORWARD -p tcp -d 192.168.49.2 --dport 80 -j ACCEPT
